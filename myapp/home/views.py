b862aaac41e226b0e1083cdde14562878bab51d6
Peter <pndungu54@gmail.com>
2017-10-18 19:27:16 +0300

    Rename main folder to home

---
+++ myapp/home/views.py
@@ -0,0 +1,9 @@
+"""Contains the route for the landing page"""
+from flask import render_template
+
+from . import home
+
+@home.route('/')
+def index():
+    """Defines the landing page"""
+    return render_template('index.html')
