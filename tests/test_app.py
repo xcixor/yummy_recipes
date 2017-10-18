8cc367da3538615857513f0be74ccac4301ef8d7
Peter <pndungu54@gmail.com>
2017-10-18 21:03:09 +0300

    Add test for the existence of an instance of the app

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import unittest
+
+from flask import current_app
+
+from myapp import create_app
+
+
+class BasicsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
