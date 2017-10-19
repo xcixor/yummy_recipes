38a9ed58f3977e581357df405760de92f64e879c
Peter <pndungu54@gmail.com>
2017-10-19 10:54:44 +0300

    Add tests for Category class

---
+++ tests/test_category.py
@@ -0,0 +1,48 @@
+"""Tests the Category class"""
+
+import unittest
+
+import sys
+
+sys.path.append("..")
+
+from myapp.app_classes import Category
+
+class TestUser(unittest.TestCase):
+    """contains the test for the category object methods
+    """
+    def setUp(self):
+        """
+        creates user objects for the tests
+        """
+        self.category = Category()
+        
+
+    def tearDown(self):
+        """
+        removes the objects after they have been used
+        """
+        del self.category
+
+    def test_category_exist(self):
+        """Tests the existence of a category"""
+        self.category.add_category('Breakfast', 'Food for morning', 'ptah')
+        result = self.category.add_category('Breakfast', 'Food for morning', 'ptah')
+        self.assertEqual(result, "Item alredy in list")
+
+    def test_delete_category(self):
+        """Test if user deleted successfully"""
+        self.category.add_category('Dessert', 'sweet food', 'ptah')
+        result = self.category.delete_category('Dessert')
+        self.assertEqual(result, "Deleted successfully")
+
+    def test_edit_category(self):
+        self.category.add_category('Dessert', 'sweet food', 'ptah')
+        result = self.category.edit_category('Brunch', 'Breakfast lunch combo', 'Dessert', 'ptah')
+        self.assertEqual(result, "Updated")
+
+    # def test_show_user_categories(self):
+    #     self.category.add_category('Cousine', 'Dont know what that is', 'aly')
+    #     result = self.category.show_categories('aly')
+    #     self.assertEqual(result, True)
+
