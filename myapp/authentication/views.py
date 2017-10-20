5d63c55cbc3c72b691f39438a717bfab367230bb
Peter <pndungu54@gmail.com>
2017-10-20 07:13:11 +0300

    Remove unused imports

--- myapp/authentication/views.py
+++ myapp/authentication/views.py
@@ -1,14 +1,9 @@
 """This module imports the authentication blueprint.
 It creates the routes for user authenication.
 """
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for
 
 from . import authentication
-
-from .forms import RegistrationForm
-
-from ..app_classes import User
-
 
 @authentication.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -19,11 +14,3 @@ def login():
 def logout():
     """Logs the user out"""
     return redirect(url_for('home.index'))
-
-@authentication.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    user = User()
-    user.register_user(form.username.data, form.password, form.password2)
-    flash('You can now login.')
-    return render_template('dashboard/dashboard.html')
