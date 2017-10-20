dd0c14036b58b290ceb1987cf106170567a44a58
Peter <pndungu54@gmail.com>
2017-10-20 08:47:48 +0300

    Add session variable to create user session

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -3,9 +3,11 @@ from flask import render_template
 
 from . import home
 
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, session, url_for, flash
 
 from . import home
+
+from flask_login import login_user, logout_user, login_required
 
 from .forms import Registration
 
@@ -14,17 +16,18 @@ from ..app_classes import User
 @home.route('/')
 def dashboard():
     """Avails the user's dashboard"""
-    return render_template('dashboard/dashboard.html')
+    return render_template('dashboard/newdash.html')
 
 @home.route('/', methods=['GET', 'POST'])
 def index():
     """Defines the landing page"""
     form = Registration()
     if form.validate_on_submit():
+        session['name'] = form.username.data
         user = User()
         if user.register_user(form.username.data, form.password.data, form.password2.data):
             # flash('You can now login.')
             return redirect(url_for('home.dashboard'))
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
