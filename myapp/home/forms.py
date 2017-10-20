d10498c7618537ebc875032f1ae8a45f30f1ba49
Peter <pndungu54@gmail.com>
2017-10-20 07:16:32 +0300

    Add flask-wtf form class

---
+++ myapp/home/forms.py
@@ -0,0 +1,18 @@
+"""Creates the form for authentication"""
+
+from flask_wtf import  FlaskForm
+
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+
+from wtforms.validators import Required, Email, Length, Email, Regexp, EqualTo
+
+from wtforms import ValidationError
+
+class Registration(FlaskForm):
+    """TThe form class created the registration form"""
+    username = StringField('Username', validators=[
+        Required(), Length(1, 64)])
+    password = PasswordField('Password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Register')
