fcf636cc1ace5fc43f1b2d0107e1dbbbe3fe1a27
Peter <pndungu54@gmail.com>
2017-10-20 13:06:44 +0300

    Edit registration function to ensure user class validates user data

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -7,27 +7,32 @@ from flask import render_template, redirect, session, url_for, flash
 
 from . import home
 
-from flask_login import login_user, logout_user, login_required
 
 from .forms import Registration
 
 from ..app_classes import User
 
-@home.route('/')
+@home.route('/dashboard')
 def dashboard():
     """Avails the user's dashboard"""
-    return render_template('dashboard/newdash.html')
+    return render_template('dashboard/dashboard.html')
 
 @home.route('/', methods=['GET', 'POST'])
 def index():
     """Defines the landing page"""
     form = Registration()
     if form.validate_on_submit():
-        session['name'] = form.username.data
+        # session['name'] = form.username.data
+        username = form.username.data
+        pass_1 = form.password.data
+        pass_2 = form.password2.data
         user = User()
-        if user.register_user(form.username.data, form.password.data, form.password2.data):
-            # flash('You can now login.')
+        if user.register_user(username, pass_1, pass_2):
+            session['username'] = username
+            flash('You can now login.')
             return redirect(url_for('home.dashboard'))
-    return render_template('index.html', form=form, name=session.get('name'))
-
+        else:
+            flash("You cant login")
+        flash("You cant login")
+    return render_template('index.html', form=form)
 
