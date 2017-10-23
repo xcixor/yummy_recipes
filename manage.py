36f791cd9da1f0e44a727bdcdeac0e7fadcb6e48
Peter <pndungu54@gmail.com>
2017-10-23 13:15:22 +0300

    Return manage.py to original state for local running of application

--- manage.py
+++ manage.py
@@ -1,18 +1,14 @@
 from myapp import create_app
 
-import os
-
 from flask_script import Manager, Shell
 
 app = create_app('default')
 
 manager = Manager(app)
 
-port = int(os.environ.get('PORT', 5000))
-
 def make_shell_context():
     """Creates the custom context for the python shell"""
-    return dict(app=app,host='0.0.0.0', port=port)
+    return dict(app=app)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
