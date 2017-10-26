8da692486cc588bd8301b04f3a1c181cca23bb52
Peter <pndungu54@gmail.com>
2017-10-26 22:53:01 +0300

    Write test for recipe class

--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -13,7 +13,8 @@ class TestUser(unittest.TestCase):
     """
     def setUp(self):
         """
-        creates user objects for the tests
+        creates a recipe object for the tests
+        
         """
         self.recipe = Recipe()
         
@@ -26,24 +27,5 @@ class TestUser(unittest.TestCase):
 
     def test_recipe_exist(self):
         """Tests the existence of a recipe"""
-        self.recipe.add_recipe('Breakfast', 'Food for morning', 'ptah')
-        result = self.recipe.add_recipe('Breakfast', 'Food for morning', 'ptah')
-        self.assertEqual(result, "Item alredy in list")
-
-    def test_delete_recipe(self):
-        
-        """Test if user deleted successfully"""
-        self.recipe.add_recipe('Dessert', 'sweet food', 'ptah')
-        result = self.recipe.delete_recipe('Dessert')
-        self.assertEqual(result, "Deleted successfully")
-
-    def test_edit_recipe(self):
-        self.recipe.add_recipe('Dessert', 'sweet food', 'ptah')
-        result = self.recipe.edit_recipe('Brunch', 'Luch and breakfast combo', 'Dessert', 'ptah')
-        self.assertEqual(result, self.recipe.recipes)
-
-    # def test_show_user_categories(self):
-    #     self.recipe.add_recipe('Cousine', 'Dont know what that is', 'aly')
-    #     result = self.recipe.show_categories('aly')
-    #     self.assertEqual(result, True)
-
+        result = self.recipe.__str__()
+        self.assertEqual(result, "Name: default recipe, Ingredients: default ingredients, Preparation: default preparation")
