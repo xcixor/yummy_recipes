356330d17f71527f38e5b871d5dcc8d0b1a250ca
Peter <pndungu54@gmail.com>
2017-10-27 06:58:08 +0300

    Edit docstrings

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,9 @@
+"""Test whether an instance of the app is created successfully and its context created"""
+
+import sys
+
+sys.path.append("..")
+
 import unittest
 
 from flask import current_app
@@ -6,13 +12,17 @@ from myapp import create_app
 
 
 class BasicsTestCase(unittest.TestCase):
+    """
+    Inherits from testcase to create the test cases
+    """
     def setUp(self):
-        """Creates the app"""
+        """Creates the objects used in the tests"""
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
 
     def tearDown(self):
+        """Cleans up the objects after the tests"""
         self.app_context.pop()
 
     def test_app_exists(self):
--- tests/test_category.py
+++ tests/test_category.py
@@ -12,11 +12,11 @@ from myapp.app_classes import Category, Recipe
 
 
 class TestUser(unittest.TestCase):
-    """contains the test for the category object methods
+    """
+    contains the test for the category object methods
     """
     def setUp(self):
-        """
-        creates a category object and two recipes and adds recipe_one to the category
+        """creates a category object and two recipes and adds recipe_one to the category
         """
         self.category_one = Category("Soups", "Goes with other foods", "James")
         self.recipe_one = Recipe('Vegetable salad', 'corriander, vegetables', \
@@ -30,9 +30,7 @@ class TestUser(unittest.TestCase):
         "A preparation", "Salads")
         
     def tearDown(self):
-        """
-        removes the objects after they have been used
-        """
+        """removes the objects after they have been used"""
         del self.category_one
         del self.recipe_one
         del self.recipe_two
@@ -42,44 +40,44 @@ class TestUser(unittest.TestCase):
         """Test whether a list of recipes is created when several items are added"""
         result = self.category_one.add_item(self.recipe_two)
         self.assertEqual(result, [{'name': 'Vegetable salad', \
-        'ingredients': 'corriander, vegetables', 'preparation': 'boil veggies and add corriander', 'owner' :'Soups'},\
-        {'name': 'Beef stew', \
-        'ingredients': 'beef, onions, pepper', 'preparation': 'Mix and boil in medium heat', 'owner': 'Soups'}])
+        'ingredients': 'corriander, vegetables', 'preparation': 'boil veggies and add corriander', \
+        'owner' :'Soups'}, {'name': 'Beef stew', 'ingredients': 'beef, onions, pepper', \
+        'preparation': 'Mix and boil in medium heat', 'owner': 'Soups'}])
 
     def test_recipe_exist(self):
         """Test whether the addition of an already existing recipe is not allowed"""
         result = self.category_one.add_item(self.recipe_one)
         self.assertEqual(result, False)
-        
+
     def test_recipe_belongs_to_user(self):
         """Test whether the recipe in the category list belongs to that particular category only"""
         self.category_one.add_item(self.recipe_two)
         self.category_one.add_item(self.recipe_three)
-        self.assertEqual(self.category_one.show_items('Soups'), [{'name': 'Vegetable salad', \
-        'ingredients': 'corriander, vegetables', 'preparation': 'boil veggies and add corriander', 'owner' :'Soups'},\
-        {'name': 'Beef stew', 'ingredients': 'beef, onions, pepper', 'preparation': 'Mix and boil in medium heat', 'owner': 'Soups'}])
-        
-
+        self.assertEqual(self.category_one.show_items('Soups'), [{'name': 'Vegetable salad',\
+        'ingredients': 'corriander, vegetables', 'preparation': 'boil veggies and add corriander', 'owner' :'Soups'},
+        {'name': 'Beef stew', 'ingredients': 'beef, onions, pepper', \
+        'preparation': 'Mix and boil in medium heat', 'owner': 'Soups'}])
+    
     def test_delete_recipe(self):
         """Test if recipe deleted successfully"""
         self.category_one.add_item(self.recipe_two)
         result = self.category_one.delete_item('Beef stew', self.category_one.item_name)
-        self.assertEqual(result,[{'name': 'Vegetable salad',\
+        self.assertEqual(result, [{'name': 'Vegetable salad',\
         'ingredients': 'corriander, vegetables', 'preparation': 'boil veggies and add corriander', 'owner' :'Soups'}])  
 
     def test_edit_recipe(self):
-        """Test if a category can be edited successfully"""
+        """Test if a recipe can be edited successfully"""
         new_recipe = self.recipe_two
         result = self.category_one.edit_item('Vegetable salad', new_recipe)
-        self.assertEqual(result,[{'name': 'Beef stew', 'ingredients': 'beef, onions, pepper', \
-        'preparation': 'Mix and boil in medium heat', 'owner': 'Soups'}])  
-
+        self.assertEqual(result, [{'name': 'Beef stew', 'ingredients': 'beef, onions, pepper', \
+        'preparation': 'Mix and boil in medium heat', 'owner': 'Soups'}])
     def test_show_user_recipes(self):
         """Returns all the recipes of the category"""
         self.category_one.add_item(self.recipe_two)
         self.category_one.add_item(self.recipe_three)
         result = self.category_one.show_items(self.category_one.name)
         self.assertEqual(result, [{'name': 'Vegetable salad', \
-        'ingredients': 'corriander, vegetables', 'preparation': 'boil veggies and add corriander', 'owner' :'Soups'},\
-        {'name': 'Beef stew', \
+        'ingredients': 'corriander, vegetables', 'preparation': 'boil veggies and add corriander', \
+        'owner' :'Soups'}, {'name': 'Beef stew', \
         'ingredients': 'beef, onions, pepper', 'preparation': 'Mix and boil in medium heat', 'owner': 'Soups'}])
+        
