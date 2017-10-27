76585a36333c99c8d2162b79e2ff717fcf4ed95f
Peter <pndungu54@gmail.com>
2017-10-27 07:01:32 +0300

    Fix pylint errors

--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -9,23 +9,19 @@ sys.path.append("..")
 from myapp.app_classes import Recipe
 
 class TestUser(unittest.TestCase):
-    """contains the test for the Recipe object methods
+    """
+    contains the test for the Recipe object methods
     """
     def setUp(self):
-        """
-        creates a recipe object for the tests
-        
-        """
+        """Creates a recipe object for the tests"""
         self.recipe = Recipe()
         
-
     def tearDown(self):
-        """
-        removes the objects after they have been used
-        """
+        """Removes the objects after they have been used"""
         del self.recipe
 
     def test_recipe_exist(self):
         """Tests the existence of a recipe"""
         result = self.recipe.__str__()
-        self.assertEqual(result, "Name: default recipe, Ingredients: default ingredients, Preparation: default preparation")
+        self.assertEqual(result, "Name: default recipe, Ingredients: default ingredients, \
+        Preparation: default preparation")
