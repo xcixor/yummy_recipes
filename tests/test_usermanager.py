7c8320e77a5a957204c1db8ed5f7ea2df9d0ccb0
Peter <pndungu54@gmail.com>
2017-10-27 07:10:43 +0300

    Add docstrings to tests methods

--- tests/test_usermanager.py
+++ tests/test_usermanager.py
@@ -1,3 +1,5 @@
+"""Contains tests for the UserManager class"""
+
 import sys
 
 sys.path.append("..")
@@ -7,9 +9,11 @@ import unittest
 from myapp.app_classes import User, UserManager
 
 class TestUser(unittest.TestCase):
-    """contains the test for a usermanager
+    """
+    contains the test for a usermanager
     """
     def setUp(self):
+        """Inititalizes the objects to test with"""
         self.user_mgr = UserManager()
         self.user_one = User("mike", "123456", "123456")
         self.user_two = User("alice", "al1234", "al1234")
@@ -17,20 +21,24 @@ class TestUser(unittest.TestCase):
         self.user_mgr.register_user(self.user_one)
 
     def tearDown(self):
+        """Cleans up after objects have been used"""
         del self.user_mgr
         del self.user_one
         del self.user_two
         del self.user_three
 
-    def test_registrater_user_twice(self):
+    def test_register_user_twice(self):
+        """Tests whether the app allows duplicate registration"""
         result = self.user_mgr.register_user(self.user_one)
         self.assertEqual(result, "User added registered")
 
     def test_passwords_match(self):
+        """Tests whether passwords provided match"""
         result = self.user_mgr.register_user(self.user_three)
         self.assertEqual(result, "The passwords do not match")
 
     def test_user_login(self):
+        """Tests whether its possible to log a registered user in"""
         self.user_mgr.register_user(self.user_two)
         result = self.user_mgr.login_user(self.user_two)
         self.assertEqual(result, "User successfully loged in")
