829d483038d3434f854e06630c936b775a366988
Peter <pndungu54@gmail.com>
2017-10-31 12:06:36 +0300

    Remove shell context not needed

--- manage.py
+++ manage.py
@@ -8,12 +8,6 @@ app = create_app('development')
 
 manager = Manager(app)
 
-def make_shell_context():
-    """Creates the custom context for the python shell"""
-    return dict(app=app)
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
-
 @manager.command
 def test():
     """Run the unit tests."""
