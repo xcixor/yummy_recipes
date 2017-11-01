58a87ebdddbcd91dc20ebcc4c4b2e84fe3fe71be
Peter <pndungu54@gmail.com>
2017-11-02 02:47:27 +0300

    Feature: Create registration form
    Registration form is more appropriate in this blueprint

--- myapp/authentication/forms.py
+++ myapp/authentication/forms.py
@@ -8,7 +8,7 @@ from wtforms.validators import Required, Email, Length, EqualTo
 
 from wtforms import ValidationError
 
-class RegistrationForm(FlaskForm):
+class LoginForm(FlaskForm):
     """
     This form class creates the login form
     """
@@ -16,3 +16,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    """
+    The form class created the registration form
+    """
+    username = StringField('Username', render_kw={"placeholder": "username"}, \
+            validators=[Required(), Length(2, 40)])
+    email = StringField('Email', render_kw={"placeholder": "Email"}, \
+            validators=[Required(), Length(1, 64), Email()])
+    password = PasswordField('Password', render_kw={"placeholder": "password"}, \
+            validators=[Required(), EqualTo('password2', message='Passwords must match.'), Length(6)])
+    password2 = PasswordField('Confirm password', render_kw={"placeholder": "password"})
+    submit = SubmitField('Register')
