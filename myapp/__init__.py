2c1fa4aa7cad0a345e265dd7dd16d7a3186749de
Peter <pndungu54@gmail.com>
2017-11-02 02:44:12 +0300

    Fix: Change authentication blueprint to be root
    Haning the authentication blueprint as the root, was important for rendering the index page

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -35,10 +35,10 @@ def create_app(configuration):
     bootstrap.init_app(app)
 
     from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint)
+    app.register_blueprint(home_blueprint, url_prefix='/authentication')
 
     from .authentication import authentication as authentication_blueprint
-    app.register_blueprint(authentication_blueprint, url_prefix='/authentication')
+    app.register_blueprint(authentication_blueprint)
 
     return app
 
