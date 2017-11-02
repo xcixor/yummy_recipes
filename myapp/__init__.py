2cc5c94eca36ce2de09b646fb027ab9af250b462
Peter <pndungu54@gmail.com>
2017-11-02 03:18:50 +0300

    Fix: Remove home blueprint
    The home blueprint has been deleted after it was no longer useful

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -34,10 +34,10 @@ def create_app(configuration):
 
     bootstrap.init_app(app)
 
-    from .home import home as home_blueprint
-    app.register_blueprint(home_blueprint, url_prefix='/authentication')
+    from myapp.dashboard import dashboard as dashboard_blueprint
+    app.register_blueprint(dashboard_blueprint, url_prefix='/dashboard')
 
-    from .authentication import authentication as authentication_blueprint
+    from myapp.authentication import authentication as authentication_blueprint
     app.register_blueprint(authentication_blueprint)
 
     return app
