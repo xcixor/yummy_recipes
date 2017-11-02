802697bd508a04e918dedb66c02b9dc89155dfe8
Peter <pndungu54@gmail.com>
2017-11-02 03:19:42 +0300

    Delete home blueprint

--- myapp/home/__init__.py
+++
@@ -1,9 +0,0 @@
-"""This file makes python treat the main directory as containing modules
-It also defines the home blueprint
-"""
-
-from flask import Blueprint
-
-home = Blueprint('home', __name__)
-
-from . import views
